dba161a898926f1bab783b1ba4a754b84bba84ad
Leriot <eratth@gmail.com>
2026-03-16 16:30:43 +0100

    Fix: BrowserRouter basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App dark-mode">
         <Routes>
           <Route path="/" element={<WeightTracker />} />
